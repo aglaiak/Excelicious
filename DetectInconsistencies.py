0bc988dec599c6bcde81c20e40bb194e03589491
aglaiak <kakoulidoumaj7@gmail.com>
2024-07-30 08:44:31 +0200

    deletion of files

--- DetectInconsistencies.py
+++
@@ -1,7 +0,0 @@
-## Check for file name inconsistencies
-
-import pandas as pd
-import os
-
-files = os.listdir()
-
--- ExcelKiller.py
+++
@@ -1,8 +0,0 @@
-## Excel Killer
-## Script that force kills all active Excel Processes
-
-import psutil
-for proc in psutil.process_iter():
-    if proc.name() == "EXCEL.EXE":
-        proc.kill()
-print("Excel closed using psutil")
